2f647d9255e63855a4aa7cc1cb8e5036e8d06d57
Caelan Kleinhans <55991831+ckleinhans@users.noreply.github.com>
2020-12-24 15:06:00 -0600

    Remove unused reference

--- src/PageAdmin.js
+++ src/PageAdmin.js
@@ -4,7 +4,6 @@ import { connect } from 'react-redux';
 import { compose } from 'redux';
 import { withRouter } from 'react-router-dom';
 import Nav from 'react-bootstrap/Nav';
-import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
